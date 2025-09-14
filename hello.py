71746221f368f47324cf835c940cffca17641053
Burlakov Nick <nick@burlakov.io>
2025-09-14 13:44:01 +0200

    F 1 (#10)

--- hello.py
+++ hello.py
@@ -11,11 +11,22 @@ def mul(a, b):
 
 
 def sum_3(a, b, c):
-    return sum(sum(a, b), c)
+    return sum_2(sum_2(a, b), c)
 
 
 def sum_4(a, b, c, d):
-    return sum(sum(a, b), sum(c, d))
+    return sum_2(sum_2(a, b), sum_2(c, d))
+
+
+INIT = 0
+
+
+def add(*args):
+    r = INIT
+    # sum everything
+    for arg in args:
+        r += arg
+    return r
 
 
 if __name__ == "__main__":
@@ -23,3 +34,12 @@ if __name__ == "__main__":
     print(sum_3(1, 2, 6))
     print(sum_4(1, 2, 3, 4))
     print(sum_3(1, 2, 6))
+    add(1, 2, 3)
+    add(1, 2, 3, 4)
+    add(1, 2, 3, 4, 5)
+
+    # for future changes
+    add2 = lambda x: add(x, 2)
+    print(add2(1))
+
+    print("all done")
