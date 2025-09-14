4c647b5ede4a96f32e331d4e20569b36ca3dcb17
Burlakov Nick <nick@burlakov.io>
2025-09-14 13:01:41 +0200

    add sum 3 + refactor (#7)

--- hello.py
+++ hello.py
@@ -2,12 +2,20 @@ def main():
     print("Hello world!")
 
 
-def sum(a, b):
+def sum_2(a, b):
     return a + b
 
 
 def mul(a, b):
     return a * b
+
+
+def sum_3(a, b, c):
+    return sum(sum(a, b), c)
+
+
+def sum_4(a, b, c, d):
+    return sum(sum(a, b), sum(c, d))
 
 
 if __name__ == "__main__":
